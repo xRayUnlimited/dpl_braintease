134a408b783d2daf88f7ae1bc29d75cd2d593dd3
Ray Suyoto <ray.suy@icloud.com>
2018-02-21 14:10:37 -0700

    first commit

---
+++ brainteasers.js
@@ -0,0 +1,14 @@
+var isString = "Ye"
+
+console.log(isString('Is this a string?'));
+
+console.log(isString([6, 3, 5, 7]));
+
+alert(typeof isString)
+
+var is
+console.log(is_Blank(''));
+
+console.log(is_Blank('abc'));
+
+
